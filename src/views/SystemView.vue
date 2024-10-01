0b9e795ec9b828cdfbfd6e4620c973c20a252220
liumingzhi <3108531642@qq.com>
2024-10-01 12:23:43 +0800

    更新

--- src/views/SystemView.vue
+++ src/views/SystemView.vue
@@ -1,9 +1,18 @@
 <template>
-  系统信息
+  <el-button type="primary" @click="login">{{text}}</el-button>
 </template>
 
 <script setup lang="ts">
+import {reactive, ref, toRefs} from "vue";
 
+const x = ref(false)
+const text = ref("系统信息")
+  const  login = () =>{
+    x.value = !x.value
+    console.log(x.value)
+    if (x.value) text.value = "关闭"
+    else text.value = "系统信息"
+  }
 </script>
 
 <style scoped lang="scss">
